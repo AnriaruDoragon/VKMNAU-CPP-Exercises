cd5c60c84b14d4b9719e67004cbcba2fc1d6e8d4
Amon <anriarudoragon@gmail.com>
2022-02-17 11:44:52 +0200

    Homework 7

--- Homework7_Ex1/Homework7_Ex1.cpp
+++ Homework7_Ex1/Homework7_Ex1.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 int main()
 {
-    // TODO
+    int m;
+    double p, S = 0;
+
+    cout << "m: ";
+    cin >> m;
+
+    for (double i = 1; i <= m; i++) {
+        if (i != 2) {
+            p = 1;
+            for (int k = 1; k <= i + 1; k++) {
+                p *= (k + 3) / k;
+                S += p * i / (i - 2);
+            }
+        }
+    }
+
+    cout << endl << "S = " << S;
 
     return 0;
 }
--- Homework7_Ex2/Homework7_Ex2.cpp
+++ Homework7_Ex2/Homework7_Ex2.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 int main()
 {
-    // TODO
+    double S = 0, x, f;
+    
+    cout << "x: ";
+    cin >> x;
+
+    for (int i = 1; i <= 7; i++) {
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            if (k == 1) f = 1;
+            f *= k;
+        }
+        S += 2 * pow(x, 2 * i - 1) / 3 * f;
+    }
+
+    cout << endl << "S = " << S;
 
     return 0;
 }
