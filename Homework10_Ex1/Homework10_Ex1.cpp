a77518ab7b608db2e088a427d9ad133b87ae598b
Amon <anriarudoragon@gmail.com>
2022-02-23 20:31:58 +0200

    Homework 10

--- Homework10_Ex1/Homework10_Ex1.cpp
+++ Homework10_Ex1/Homework10_Ex1.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 int main()
 {
-    // TODO
+    double sum = 0, arr[8];
+
+    for (int i = 0; i <= 7; i++) {
+        cout << "Число " << i+1 << ": ";
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    cout << endl << "Sum = " << sum;
 
     return 0;
 }
