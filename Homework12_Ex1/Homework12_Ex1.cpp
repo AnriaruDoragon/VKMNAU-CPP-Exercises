694c6d1e6783f7b4d7db8dfddec6d7fb3b7ab16a
Amon <anriarudoragon@gmail.com>
2022-03-15 16:08:05 +0200

    HW 2 Done

--- Homework12_Ex1/Homework12_Ex1.cpp
+++ Homework12_Ex1/Homework12_Ex1.cpp
@@ -3,7 +3,19 @@ using namespace std;
 
 int main()
 {
+    int ar1[10], sum = 0;
+    float ar2[10];
 
+    for (int i = 0; i < 10; i++) {
+        cout << i + 1 << " число: ";
+        cin >> ar1[i];
+        if (i % 2 != 0) sum += ar1[i];
+    }
+
+    for (int i = 0; i < 10; i++) {
+        ar2[i] = (float)ar1[i] / sum;
+        cout << endl << i + 1 << " = " << ar2[i];
+    }
 
     return 0;
 }
--- Homework12_Ex2/Homework12_Ex2.cpp
+++ Homework12_Ex2/Homework12_Ex2.cpp
@@ -3,7 +3,24 @@ using namespace std;
 
 int main()
 {
-    
+    const int N = 10;
+    double ar[N];
+
+    for (int i = 0; i < N; i++) {
+        cout << i + 1 << " число: ";
+        cin >> ar[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (i + 1 == N) {
+            cout << endl << "Масив є впорядкований.";
+            break;
+        }
+        if (ar[i] > ar[i + 1]) {
+            cout << endl << "Масив не є впорядкованим.";
+            break;
+        }
+    }
 
     return 0;
 }
