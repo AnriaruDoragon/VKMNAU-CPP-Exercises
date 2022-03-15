27b0c2b7db6624461e1e75434ba250e577946b4b
Amon <anriarudoragon@gmail.com>
2022-03-15 14:01:31 +0200

    HW 11 Done

--- Homework11_Ex1/Homework11_Ex1.cpp
+++ Homework11_Ex1/Homework11_Ex1.cpp
@@ -3,7 +3,16 @@ using namespace std;
 
 int main()
 {
+    int n = 0, x[12];
 
+    for (int i = 0; i < 12; i++) {
+        cout << i + 1 << " число: ";
+        cin >> x[i];
+
+        if (x[i] > -10 && x[i] < 30 && x[i] % 2 == 0) n++;
+    }
+
+    cout << endl << "n = " << n;
 
     return 0;
 }
--- Homework11_Ex2/Homework11_Ex2.cpp
+++ Homework11_Ex2/Homework11_Ex2.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main()
 {
+    const int N = 10;
+    double x[N];
+    double m = 1, n = 0, max = -DBL_MAX;
 
+    for (int i = 0; i < N; i++) {
+        cout << i + 1 << " число: ";
+        cin >> x[i];
+
+        if (x[i] != 0) m *= x[i];
+        if (x[i] < 0) n++;
+        if (x[i] > max) max = x[i];
+    }
+
+    cout << endl << "Добуток ненульових елементів = " << m 
+        << endl << "Кількість від’ємних елементів = " << n 
+        << endl << "Максимальний елемент = " << max;
 
     return 0;
 }
