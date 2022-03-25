8ede558ee5571eadf723f34678f89058ac62899e
Amon <anriarudoragon@gmail.com>
2022-03-25 13:29:04 +0200

    HW 15 Done

--- Homeworks/15/Homework15_Ex1.cpp
+++ Homeworks/15/Homework15_Ex1.cpp
@@ -3,7 +3,28 @@ using namespace std;
 
 int main()
 {
-    // TODO
+    const int N = 10;
+    double ar[N], tmp;
+
+    for (int i = 0; i < N; i++) {
+        cout << i + 1 << " число: ";
+        cin >> ar[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N-1; j++) {
+            if (ar[j] < ar[j+1]) {
+                tmp = ar[j];
+                ar[j] = ar[j+1];
+                ar[j+1] = tmp;
+            }
+        }
+    }
+    
+    cout << endl;
+    for (int i = 0; i < N; i++) {
+        cout << ar[i] << ", ";
+    }
 
     return 0;
 }
