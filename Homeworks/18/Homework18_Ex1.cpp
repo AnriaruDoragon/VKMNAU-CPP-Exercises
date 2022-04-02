b81d63e3798ae460f98b675b51209839009c2461
Amon <anriarudoragon@gmail.com>
2022-04-03 00:28:56 +0300

    HW 18 19 Done

--- Homeworks/18/Homework18_Ex1.cpp
+++ Homeworks/18/Homework18_Ex1.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 int main()
 {
-    // TODO
+    int sum;
+    const string names[3] = { "Антонов", "Бондарь", "Василенко" };
+    const int table[3][5] = {
+        { 95, 95, 95, 95, 95 },
+        { 90, 85, 90, 95, 95 },
+        { 60, 75, 65, 80, 80 }
+    };
+
+    for (int i = 0; i < 3; i++) {
+        sum = 0;
+        for (int j = 0; j < 5; j++) {
+            sum += table[i][j];
+        }
+        cout << names[i] << " - " << sum << endl;
+    }
 
     return 0;
 }
--- Homeworks/19/Homework19_Ex1.cpp
+++ Homeworks/19/Homework19_Ex1.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 int main()
 {
-    // TODO
+    int sum;
+    int ar[5][7];
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 7; j++) {
+            cout << "ar[" << i+1 << "][" << j+1 << "]: ";
+            cin >> ar[i][j];
+        }
+    }
+
+    for (int j = 0; j < 7; j++) {
+        sum = 0;
+        for (int i = 0; i < 5; i++) {
+            if (ar[i][j] < 0) sum += ar[i][j];
+        }
+        cout << endl << j+1 << " = " << sum;
+    }
 
     return 0;
 }
