c1f638555c586582feec7c83daef5c7125ac7f84
Amon <anriarudoragon@gmail.com>
2022-04-05 17:12:50 +0300

    HW 20 21 Done

--- Homeworks/20/Homework20_Ex1.cpp
+++ Homeworks/20/Homework20_Ex1.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 int main()
 {
-    // TODO
+    int mul;
+    const int ar[4][7] = {
+        { 0, 15, 19, -38, 1, 99, 5 },
+        { 18, -10, 10, 25, 0, 0, 0 },
+        { 0, -15, 85, 113, -1, 1, 7 },
+        { 199, -66, -69, 0, 7, 55, 23 }
+    };
+
+    for (int j = 0; j < 7; j++) {
+        if (j%2 == 0) {
+            mul = 1;
+            for (int i = 0; i < 4; i++) {
+                if (ar[i][j] != 0) mul *= ar[i][j];
+            }
+            cout << j << " = " << mul << endl;
+        }
+    }
 
     return 0;
 }
--- Homeworks/21/Homework21_Ex1.cpp
+++ Homeworks/21/Homework21_Ex1.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <iomanip>
+#include <math.h>
 using namespace std;
 
 int main()
 {
-    // TODO
+    int count = 0;
+    float matrix[5][3];
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 3; j++) {
+            matrix[i][j] = sqrt(abs(pow(i, 3)-pow(j, 2)))/log10(i+j+1);
+            if (matrix[i][j] < 2) count++;
+            cout << setw(15) << matrix[i][j];
+        }
+        cout << endl;
+    }
+
+    cout << endl << "Кількість елементів менше за 2 = " << count;
 
     return 0;
 }
